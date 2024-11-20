b960c0f1acb219358afd056e7bebe766acf6fc86
Miste Ry <47310669+misteryTech@users.noreply.github.com>
2024-11-20 21:14:46 +0800

    update encoder
    update encoder

--- admin_panel/restore_encoder_process.php
+++ admin_panel/restore_encoder_process.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id'])) {
     $encoderId = mysqli_real_escape_string($conn, $_POST['id']);
 
     // Update the encoder's status to 'archived'
-    $query = "UPDATE encoder SET status = 'unarchived' WHERE encoder_id = '$encoderId'";
+    $query = "UPDATE encoder SET status = 'unarchive' WHERE encoder_id = '$encoderId'";
     if (mysqli_query($conn, $query)) {
         echo json_encode(["success" => "Encoder restore successfully."]);
     } else {
